a1fb262619c750e0ab67d1354db3cb1c47dfaf2c
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-11-11 09:52:55 +0100

    tags

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyscribble',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(include=["pyscribble*"]),
     author='Thomas Schmelzer',
     author_email='thomas.schmelzer@gmail.com',
