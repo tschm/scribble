2b7e2a8d43f3f96aedfdf90ab050da61f938018e
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2024-12-03 20:48:19 +0400

    remove docker

--- marimo/scribble.py
+++ marimo/scribble.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.9.28"
+__generated_with = "0.9.29"
 app = marimo.App()
 
 
@@ -9,53 +9,22 @@ def __(__file__):
     from pathlib import Path
 
     import matplotlib.pyplot as plt
-    import numpy as np
-    import pandas as pd
 
-    from pyscribble.scribble import scribble
+    from pyscribble.scribble import wedding
 
     path = Path(__file__).parent
-    return Path, np, path, pd, plt, scribble
+    return Path, path, plt, wedding
 
 
 @app.cell
-def __(plt, scribble):
-    def wedding(word, f, title):
-        fig, axes = plt.subplots(
-            nrows=2, ncols=1, figsize=(8.27, 11.69), subplot_kw={"aspect": "equal"}
-        )
-
-        scribble(axes[1], text=word, f=f, title=title, linewidth=2, color="blue")
-
-        axes[0].set_axis_off()
-        axes[0].text(
-            0.5,
-            0.5,
-            "{name}\n{function}".format(name=word, function=f),
-            horizontalalignment="right",
-            rotation=180,
-        )
-
-        plt.show()
-
-    return (wedding,)
-
-
-@app.cell
-def __(wedding):
-    text = "Hans Dampf"
-    title = "My wedding"
-    function = "tanh((-1+2j)*z)"
-    wedding(word=text, title=title, f=function)
-    return function, text, title
-
-
-@app.cell
-def __(path, pd, wedding):
-    frame = pd.read_csv(path / "input" / "names.csv", header=0)
-    for index, row in frame.iterrows():
-        wedding(word=row["Name"], f=row["Function"], title="Lydia & Thomas")
-    return frame, index, row
+def __(path, plt, wedding):
+    with open(path / "input" / "names.csv") as f:
+        for line in f:
+            name, func = line.split(",")
+            wedding(word=name, f=func, title="Wedding Lydia & Thomas")
+            # plt.savefig(f"{name}.png")
+            plt.show()
+    return f, func, line, name
 
 
 if __name__ == "__main__":
--- pyscribble/scribble.py
+++ pyscribble/scribble.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numexpr as ne
 import numpy as np
 
@@ -31,3 +32,22 @@ def scribble(axes, text, f, title="Lydia & Thomas, August 17, Rhodes House", **k
             axes.plot(t.real, t.imag, **kwargs)
 
     axes.set_title(title)
+
+
+def wedding(word, f, title):
+    fig, axes = plt.subplots(
+        nrows=2, ncols=1, figsize=(8.27, 11.69), subplot_kw={"aspect": "equal"}
+    )
+
+    scribble(axes[1], text=word, f=f, title=title, linewidth=2, color="blue")
+
+    axes[0].set_axis_off()
+    axes[0].text(
+        0.5,
+        0.5,
+        "{name}\n{function}".format(name=word, function=f),
+        horizontalalignment="right",
+        rotation=180,
+    )
+
+    return axes
